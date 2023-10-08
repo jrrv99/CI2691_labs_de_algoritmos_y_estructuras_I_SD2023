bc60cd7b2dfdcbf3e15d404ecac654107fd88ad9
José Riobueno <jrrv99@gmail.com>
2023-10-07 21:44:26 -0400

    fix(LabSem2/EsNumeroPrimo): :recycle: add check for numbers less than 2 which are not prime numbers

--- LabSem2/EsNumeroPrimo.java
+++ LabSem2/EsNumeroPrimo.java
@@ -2,6 +2,11 @@ class EsNumeroPrimo {
     public static void esNumeroPrimo(int n) {
         int i = 2;
 
+        if (n < 2) {
+            System.out.println(n + " no es primo");
+            return;
+        }
+
         while (i < n) {
             if (n%i == 0){ 
                 System.out.println(n + " no es primo");
