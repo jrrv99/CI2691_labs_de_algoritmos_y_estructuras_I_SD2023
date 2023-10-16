ea0d6425ca7c3e2dcbb14a0bbcc5a25718412941
José Riobueno <jrrv99@gmail.com>
2023-10-15 23:35:32 -0400

    feat(LabSem3): :sparkles: add JML annotations to MinimoDeCuatro

--- LabSem3/MinimoDeCuatro.java
+++ LabSem3/MinimoDeCuatro.java
@@ -1,6 +1,15 @@
 class MinimoDeCuatro {
+    public static int min;
+
+    //@ requires Integer.MIN_VALUE <= num1 && num1 <= Integer.MAX_VALUE;
+    //@ requires Integer.MIN_VALUE <= num2 && num2 <= Integer.MAX_VALUE;
+    //@ requires Integer.MIN_VALUE <= num3 && num3 <= Integer.MAX_VALUE;
+    //@ requires Integer.MIN_VALUE <= num4 && num4 <= Integer.MAX_VALUE;
+    //@ ensures min <= num1 && min <= num2 && min <= num3 && min <= num4;
+    //@ ensures min == Math.min(Math.min(num1, num2), Math.min(num3, num4));
+    //@ ensures Integer.MIN_VALUE <= min && min <= Integer.MAX_VALUE;
     public static void minimoDeCuatro(int num1, int num2, int num3, int num4) {
-        int min = num1;
+        min = num1;
 
         if (num2 < min) {
             min = num2;
