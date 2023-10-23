5f13b7a8a948f0e43912c7899e8f5e0570408820
José Riobueno <jrrv99@gmail.com>
2023-10-22 22:57:49 -0400

    feat(LabSem4): :sparkles: add JML annotations to EsPalindromo

--- LabSem4/EsPalindromo.java
+++ LabSem4/EsPalindromo.java
@@ -1,12 +1,12 @@
 class EsPalindromo {
-    public static void main(String[] args) {
-        char[] letters = args[0].toCharArray();
-        System.out.println(esPalindromo(letters));
-    }
-
+    //@ requires sec != null;
+    //@ ensures \result == (\forall int j; 0 <= j && j < (sec.length / 2); sec[j] == sec[(sec.length - 1) - j]);
     public static boolean esPalindromo(char[] sec) {
         int i = 0;
 
+        //@ maintaining 0 <= i <= ((sec.length)/ 2);
+		//@ maintaining (\forall int j; 0 <= j && j <= i; sec[i] == sec[(sec.length - 1) - j]);
+        //@ decreases sec.length/2 - i;
         while(i < sec.length / 2) {
             if (sec[i] != sec[(sec.length - 1) - i]) return false;
 
