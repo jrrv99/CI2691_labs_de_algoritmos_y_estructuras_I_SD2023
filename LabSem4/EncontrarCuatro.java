37039c9d729f5b32e309286c260787b767fe116f
José Riobueno <jrrv99@gmail.com>
2023-10-22 23:02:26 -0400

    feat(LabSem4): :sparkles: add JML annotations to EncontrarCuatro

--- LabSem4/EncontrarCuatro.java
+++ LabSem4/EncontrarCuatro.java
@@ -1,21 +1,13 @@
 class EncontrarCuatro {
     private static final int NUMBER_TO_FIND = 4;
 
-    public static void main (String[] args) {
-        int i = 0;
-        int[] sec = new int[args.length];
-
-        while (i < args.length) {
-            sec[i] = Integer.parseInt(args[i]);
-            i++;
-        }
-        
-        System.out.println(encontrarCuatro(sec));
-    }
-
+    // requires sec != null;
+    //@ ensures \result >= -1 && \result < sec.length;
     public static int encontrarCuatro(int[] sec) {
         int index = 0;
-
+        
+        //@ maintaining 0 <= index && index <= sec.length;
+        //@ decreasing sec.length - index;
         while(index < sec.length) {
             if (sec[index] == NUMBER_TO_FIND) return index;
             index++;
