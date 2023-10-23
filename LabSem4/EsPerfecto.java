6af07e3874cb0fbbdd4804a6c3791283c7143bf9
José Riobueno <jrrv99@gmail.com>
2023-10-22 22:26:22 -0400

    feat(LabSem4): :sparkles: add JML annotations to EsPerfecto

--- LabSem4/EsPerfecto.java
+++ LabSem4/EsPerfecto.java
@@ -1,11 +1,11 @@
 class EsPerfecto {
-    public static void main(String[] args){
-        System.out.println(esPerfecto(Integer.parseInt(args[0])));
-    }
-
+    //@ requires N > 0; 
+    //@ ensures \result == ((\sum int j; 0 < j && j < N && N % j == 0; j) == N);
     public static boolean esPerfecto(int N) {
         int divisor = 1, suma = 0;
 
+        //@ maintaining suma == (\sum int i; 0 < i && i < divisor && N % i == 0; i);
+        //@ decreasing N - divisor;
         while(divisor < N) {
             if (N % divisor == 0) suma += divisor;
             if (suma > N) return false;
