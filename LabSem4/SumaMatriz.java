54f87adaf91f16d048a125b51d659beedb859a63
José Riobueno <jrrv99@gmail.com>
2023-10-22 23:07:51 -0400

    feat(LabSem4): :sparkles: add JML annotations to SumaMatriz

--- LabSem4/SumaMatriz.java
+++ LabSem4/SumaMatriz.java
@@ -1,16 +1,13 @@
 class SumaMatriz {
-    public static void main(String[] args) {
-        int[][] matriz = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-        };
-
-        System.out.println(sumaMatriz(matriz));
-    }
+    //@ require matriz != null;
+    //@ requires matriz.length >= matriz[0].length >= 0;
+	//@ requires (\forall int x; 0 <= x && x < matriz.length; matriz[x].length == matriz[0].length);
+	//@ ensures \result == (\sum int i; 0 <= i && i < matriz.length; (\sum int j; 0 <= j && j < matriz[i].length; matriz[i][j]));
     public static int sumaMatriz(int[][] matriz) {
         int row = 0, column, sum = 0;
 
+        //@ maintaining suma == (\sum int j; 0 <= j && j < row; (\sum int k; 0 <= k && k < column; matriz[j][k]));
+        //@ decreasing matriz.length - row;
         while(row < matriz.length) {
             column = 0;
             while (column < matriz[row].length) {
