870f9d82a956cd7270fb713b3826cb6fbabcbc3b
José Riobueno <jrrv99@gmail.com>
2023-10-22 22:33:34 -0400

    feat(LabSem4): :sparkles: add JML annotations to PromedioSecuencia

--- LabSem4/PromedioSecuencia.java
+++ LabSem4/PromedioSecuencia.java
@@ -1,21 +1,13 @@
 class PromedioSecuencia {
-    public static void main(String[] args){
-        double[] sec = new double[args.length];
-        int i = 0;
-
-        while(i < args.length) {
-            sec[i] = Double.parseDouble(args[i]);
-
-            i++;
-        }
-
-        System.out.println(promedioSecuencia(sec));
-    }
-
+    //@ requires sec != null && sec.length > 0;
+    //@ ensures \java_math(\result == (\sum int i; 0 <= i && i < sec.length; sec[i]) / sec.length);
     public static double promedioSecuencia(double[] sec) {
         int i = 0;
         double sum = 0;
 
+        //@ maintaining 0 <= i && i <= sec.length;
+        //@ maintaining \java_math(sum == (\sum int j; 0 <= j && j < i; sec[j]));
+        //@ decreasing sec.length - i; 
         while (i < sec.length) {
             sum += sec[i];
 
