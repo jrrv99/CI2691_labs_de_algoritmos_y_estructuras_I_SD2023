13aeb05cd33a91cac3e99e00ad8920e99f4fe4e9
José Riobueno <jrrv99@gmail.com>
2023-10-22 22:48:31 -0400

    feat(LabSem4): :sparkles: add JML annotations to ContarPrimos

--- LabSem4/ContarPrimos.java
+++ LabSem4/ContarPrimos.java
@@ -1,22 +1,18 @@
 class ContarPrimos {
-    public static void main(String[] args){
-        int[] sec = new int[args.length];
-        int i = 0;
-
-        while(i < args.length) {
-            sec[i] = Integer.parseInt(args[i]);
-            i++;
-        }
-
-        System.out.println(contarPrimos(sec));
-    }
-    
+    //@ requires sec != null;
+    //@ requires (\forall int k; 0 <= k && k < sec.length; sec[k] > 0);
+    //ensures \result == (\num_of int i; 0 < i && i <= sec.length; (\forall int k; 2 < k && k < sec[i]; sec[i]%k != 0));
     public static int contarPrimos (int[] sec) {
         int i = 0, divider = 2, count = 0;
 
+        //@ maintaining 0 <= i && i <= sec.length;
+        //@ decrease sec.length - i
         while (i < sec.length){
             if (sec[i] <= 1) continue; // No es primo
 
+            //@ maintaining 2 <= divider && divider <= sec[i] + 1;
+            //@ maintaining (\forall int k; 1 < k && k < i; sec[i]%k != 0);
+            //@ decrease sec[i] - divider;
             while (divider < sec[i]) {
                 if (sec[i] % divider == 0) { // No es primo
                     divider = -1;
