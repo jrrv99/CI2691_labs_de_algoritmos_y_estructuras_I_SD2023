81e8a49ca5746f1d79d87aa6a5126be3674bdaff
José Riobueno <jrrv99@gmail.com>
2023-10-22 22:10:53 -0400

    feat(LabSem4): :sparkles: add JML annotations to ContarDivisores

--- LabSem4/ContarDivisores.java
+++ LabSem4/ContarDivisores.java
@@ -1,9 +1,17 @@
 class ContarDivisores {
+    //@ requires number > 0;
+    //@ ensures \result >= 0;
+    //@ ensures \result <= number;
+    //@ ensures \result == (\num_of int i; 0 < i && i <= number; number % i == 0);
     public static int contarDivisores(int number) {
         int divisor = 1, count = 0;
-
-        while(divisor <= number) {
-            if(number % divisor == 0) count++;
+        
+        //@ maintaining 1 <= divisor <= number + 1;
+        //@ maintaining count <= divisor - 1;
+        //@ maintaining count == (\num_of int i; 0 < i & i < divisor; number % i == 0);
+        //@ decreases number - divisor + 1;
+        while (divisor <= number) {
+            if (number % divisor == 0) count++;
 
             divisor++;
         }
