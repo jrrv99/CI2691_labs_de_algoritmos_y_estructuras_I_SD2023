2d7c9e639cfb8e9cd2a4954696a6a6405fd36a9a
José Riobueno <jrrv99@gmail.com>
2023-10-27 13:10:14 -0400

    Add jml to Desviacion Estandar

--- Examen 1/DesviacionEstandar.java
+++ Examen 1/DesviacionEstandar.java
@@ -1,8 +1,17 @@
 class DesviacionEstandar {
+    public static double mediaAritmetica = 0.0;
+
+    //@ requires S != null;
+    //@ requires S.length > 0;
+    //@ ensures mediaAritmetica == ((\sum int i; 0 <= i && i < S.length; S[i])/S.length);
+    //@ ensures \result == Math.sqrt((\sum int i; 0 <= i && i < S.length; Math.pow(S[i] - ((\sum int j; 0 <= j && j < S.length; S[j])/S.length), 2)) / S.length);
     public static double desviacionEstandar (int[] S) {
         int N = S.length, i = 0;
-        double desviacionEstandar = 0.0, mediaAritmetica = 0.0, diferencia;
+        double desviacionEstandar = 0.0, diferencia;
 
+        //@ maintaining 0 <= i && i <= N;
+        //@ maintaining mediaAritmetica == (\sum int j; 0 <= j && j <= i; S[i]);
+        //@ decreasing N - i;
         while (i < N) {
             mediaAritmetica += S[i];
             i++;
@@ -11,6 +20,9 @@ class DesviacionEstandar {
         mediaAritmetica = mediaAritmetica / N;
         i=0;
 
+        //@ maintaining 0 <= i && i <= N;
+        //@ maintaining desviacionEstandar == (\sum int j; 0 <= j && j <= i; Math.pow(diferencia, 2));
+        //@ decreasing N - i;
         while (i < N) {
             diferencia = S[i] - mediaAritmetica;
             desviacionEstandar += Math.pow(diferencia, 2);
@@ -19,6 +31,6 @@ class DesviacionEstandar {
         }
 
         return Math.sqrt(desviacionEstandar / N);
-
     }
 }
+
