553cea3e76c302cae80bae2217cd5806de5feec8
José Riobueno <jrrv99@gmail.com>
2023-11-12 16:32:37 -0400

    feat(LabSem6): :sparkles: add diferenciaEntreFechas solution

--- LabSem6/Cliente.java
+++ LabSem6/Cliente.java
@@ -1,15 +1,59 @@
 class Cliente  {
-    public static void main(String[] args) {
-        System.out.println("\nesDivisor - START TEST");
+    public static void testEsDivisor() {
         assert Utilidades.esDivisor(234567, 3) : "Error, 234567 es divisible por 3";
         assert !Utilidades.esDivisor(234568, 7) : "Error, 234568 no es divisible por 7";
-        System.out.println("esDivisor - END TEST");
+    }
 
-        System.out.println("\nesPrimo - START TEST");
+    public static void testEsPrimo() {
         assert Utilidades.esPrimo(7993) : "Error, 7993 es primo";
         assert Utilidades.esPrimo(7019) : "Error, 7019 es primo";
         assert !Utilidades.esPrimo(7020) : "Error, 7020 no es primo";
         assert !Utilidades.esPrimo(-3) : "Error, -3 no es primo, El número debe ser positivo";
+    }
+
+    public static void testDiferenciaEntreFechas() {
+        int[] correcto = new int[3];
+        int[] resultado = new int[3];
+        
+        resultado = Utilidades.diferenciaEntreFechas(30,04,1996,27,02,1996);
+        correcto[0] = 0; 
+        correcto[1] = 2;
+        correcto[2] = 3;
+        assert checkFechas(correcto, resultado) : "Error en la primera prueba de diferencia de fechas";
+
+        resultado = Utilidades.diferenciaEntreFechas(02,03,1996,27,02,1996);
+        correcto[0] = 0; 
+        correcto[1] = 0;
+        correcto[2] = 4;
+        assert checkFechas(correcto, resultado) : "Error en la segunda prueba de diferencia de fechas";
+        
+        resultado = Utilidades.diferenciaEntreFechas(03,03,2022,01,01,2020);
+        correcto[0] = 2; 
+        correcto[1] = 2;
+        correcto[2] = 2;
+        assert checkFechas(correcto, resultado) : "Error en la tercera prueba de diferencia de fechas";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("\nesDivisor - START TEST");
+        testEsDivisor();
+        System.out.println("esDivisor - END TEST");
+
+        System.out.println("\nesPrimo - START TEST");
+        testEsPrimo();
         System.out.println("esPrimo - END TEST");
+
+        System.out.println("\ndiferenciaEntreFechas - START TEST");
+        testDiferenciaEntreFechas();
+        System.out.println("diferenciaEntreFechas - END TEST");
+    }
+
+    private static boolean checkFechas(int[] fcorrecta, int[] fobtenida) {
+        if ((fcorrecta[0] != fobtenida[0]) && (fcorrecta[1] != fobtenida[1]) && (fcorrecta[2] != fobtenida[2]) ) {
+            System.out.println("\nDiscrepancia\n, diferencia correcta: "+fcorrecta[0]+" anios, "+fcorrecta[1]+" meses, "+fcorrecta[2]+" dias");
+            System.out.println("\nDiscrepancia\n, diferencia dada: "+fobtenida[0]+" anios, "+fobtenida[1]+" meses, "+fobtenida[2]+" dias");
+            return false;
+        }
+        return true;
     }
 }
--- LabSem6/Utilidades.java
+++ LabSem6/Utilidades.java
@@ -1,30 +1,32 @@
 public class Utilidades {
 
-    //@ requires Integer.MIN_VALUE <= n < Integer.MAX_VALUE;
-    //@ ensures Integer.MIN_VALUE <= \result <= Integer.MAX_VALUE; 
-    //@ ensures \result == n + 1;
+    /*@ requires Integer.MIN_VALUE <= n < Integer.MAX_VALUE;
+      @ ensures Integer.MIN_VALUE <= \result <= Integer.MAX_VALUE; 
+      @ ensures \result == n + 1;
+      @*/
     public static /*@ pure @*/  int incremento(int n) {
 	    return n + 1;
     }
     
-    //@ requires 1 <= exp < 6;
-    //@ requires -10 <= base < 10;
-    //@ ensures Integer.MIN_VALUE <= \result <= Integer.MAX_VALUE;
-    //@ ensures \result == base * spec_exponenciar(base, exp-1);
-    //@ also
-    //@ requires exp == 0;
-    //@ ensures \result == 1;
-    //@ public static model pure int spec_exponenciar(int base, int exp) {
-    //@	if (exp == 0) 
-    //@	    return 1;
-    //@	 else 
-    //@	    return base*spec_exponenciar(base, exp-1);
-    //@ }
+    /*@ requires 1 <= exp < 6;
+      @ requires -10 <= base < 10;
+      @ ensures Integer.MIN_VALUE <= \result <= Integer.MAX_VALUE;
+      @ ensures \result == base * spec_exponenciar(base, exp-1);
+      @ also
+      @ requires exp == 0;
+      @ ensures \result == 1;
+      @ public static model pure int spec_exponenciar(int base, int exp) {
+      @	if (exp == 0) 
+      @	    return 1;
+      @	 else 
+      @	    return base*spec_exponenciar(base, exp-1);
+      @ }
 
-    //@ requires 0 <= exp < 6;
-    //@ requires -10 <= base < 10;
-    //@ ensures Integer.MIN_VALUE <= \result <= Integer.MAX_VALUE;
-    //@ ensures \result == spec_exponenciar(base, exp);
+      @ requires 0 <= exp < 6;
+      @ requires -10 <= base < 10;
+      @ ensures Integer.MIN_VALUE <= \result <= Integer.MAX_VALUE;
+      @ ensures \result == spec_exponenciar(base, exp);
+      @*/
     public static /*@ pure @*/ int exponenciar(int base, int exp) {
         if (exp == 0)
             return 1;
@@ -40,6 +42,30 @@ public class Utilidades {
             resultado = base * resultado;
         }
         return resultado;
+    }
+
+    /*@ requires 0 <= year <= Integer.MAX_VALUE;
+      @ ensures \result == (year % 4 == 0 & !(year % 100 == 0 & year % 400 != 0));
+      @*/
+    public static /*@ pure */ boolean esAnioBisiesto(int year) {
+        return (year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0));
+    }
+
+    /*@ requires 0 < year <= Integer.MAX_VALUE;
+      @ requires 1 <= month <= 12;
+      @ ensures \result == 29 <== (month == 2) && (year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0) == true);
+      @ ensures \result == 28 <== (month == 2) && (year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0) == false);
+      @ ensures \result == 30 <== (month != 2) && (month == 4 || month == 6 || month == 9 || month == 11);
+      @ ensures \result == 31 <== (month != 2) && !(month == 4 || month == 6 || month == 9 || month == 11);
+      @*/
+    public static /*@ pure */ int getLastDayOfMonth(int month, int year) {
+        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        if (month == 2 && esAnioBisiesto(year)) {
+            return 29;
+        }
+        
+        return daysInMonth[month - 1];
     }
 
     /*@ requires Integer.MIN_VALUE <= n <= Integer.MAX_VALUE;
@@ -67,5 +93,41 @@ public class Utilidades {
             i++;
         }
         return true;
+    }
+
+    /*@ requires 0 < year1 <= Integer.MAX_VALUE;
+      @ requires 0 < year2 <= Integer.MAX_VALUE;
+      @ requires 1 <= month1 <= 12;
+      @ requires 1 <= month2 <= 12;
+      @ requires 1 <= day1 <= 31;
+      @ requires 1 <= day2 <= 31;
+      @ requires year1 == year2 ? month2 <= month1 && (month1 == month2 ? day2 <= day1 : true) : true;
+      @ ensures \result != null;
+      @ ensures \result instanceof int[];
+      @ ensures \result.length == 3;
+      @*/
+    public static /*@ pure */ int[] diferenciaEntreFechas (
+        int day1, int month1, int year1,
+        int day2, int month2, int year2
+    ) {
+        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int lastDayOfPreviousMonth;
+        int[] diff = new int[3]; // [DD, MM, YYYY]
+
+        diff[0] = year1 - year2;
+        diff[1] = month1 - month2;
+        diff[2] = day1 - day2;
+
+        if (diff[1] < 0) {
+            diff[0]--; // down a year
+            diff[1] += 12; // move the cursor 12 months back
+        }
+
+        if (diff[2] < 0) {
+            diff[1]--; // down a month
+            diff[2] += (month1 == 2 && esAnioBisiesto(year1)) ? 29 : daysInMonth[month1 - 1];  // move the cursor the days length of previous month back
+        }
+        
+        return diff;
     }
 }
