dfb282db3fde39acfdd5efe86203dccafdd6e237
José Riobueno <jrrv99@gmail.com>
2023-11-12 13:31:05 -0400

    feat(LabSem6): :sparkles: add esPrimo solution

--- LabSem6/Cliente.java
+++ LabSem6/Cliente.java
@@ -1,8 +1,15 @@
 class Cliente  {
     public static void main(String[] args) {
-        System.out.println("START esDivisor TEST");
+        System.out.println("\nesDivisor - START TEST");
         assert Utilidades.esDivisor(234567, 3) : "Error, 234567 es divisible por 3";
         assert !Utilidades.esDivisor(234568, 7) : "Error, 234568 no es divisible por 7";
-        System.out.println("END esDivisor TEST");
+        System.out.println("esDivisor - END TEST");
+
+        System.out.println("\nesPrimo - START TEST");
+        assert Utilidades.esPrimo(7993) : "Error, 7993 es primo";
+        assert Utilidades.esPrimo(7019) : "Error, 7019 es primo";
+        assert !Utilidades.esPrimo(7020) : "Error, 7020 no es primo";
+        assert !Utilidades.esPrimo(-3) : "Error, -3 no es primo, El número debe ser positivo";
+        System.out.println("esPrimo - END TEST");
     }
 }
