2966d6a070523010ab27ed202ba4e8bfe15071a8
José Riobueno <jrrv99@gmail.com>
2023-11-17 11:19:54 -0400

    feat(Examen 2): :sparkles: add PromedioPares jml specifications

--- Examen 2/PromedioPares.java
+++ Examen 2/PromedioPares.java
@@ -1,9 +1,19 @@
 public class PromedioPares {
+    /*@ requires arr.length > 0;
+      @ requires (\forall int k; 0 <= k && k < arr.length; arr[k] > 0);
+      @ ensures ((\num_of int k; 0 <= k && k < arr.length; arr[k]%2 == 0) == 0 && \java_math(Math.abs((\result - 0.0)*1.0) < 0.001)) || ((\num_of int k; 0 <= k && k < arr.length; arr[k]%2 == 0) != 0 && \java_math(Math.abs(\result - (\sum int k; 0 <= k && k < arr.length && arr[k]%2 == 0; arr[k])/((\num_of int k; 0 <= k && k < arr.length; arr[k]%2 == 0)*1.0)) < 0.001));
+      @*/
     public static /*@ pure @*/ double promedioPares(int[] arr) {
         int i = 0,
             evens_sum = 0,
             evens_count = 0;
 
+        /*@ maintaining 0 <= i <= arr.length;
+          @ maintaining 0 <= evens_count <= i;
+          @ maintaining evens_sum == (\sum int k;  0 <= k && k < i && arr[k]%2==0; arr[k]);
+          @ maintaining evens_count == (\num_of int k; 0 <= k && k < i; arr[k]%2==0);
+          @ decreasing arr.length - i;
+          @*/
         while (i < arr.length) {
             if (arr[i] % 2 == 0) {
                 evens_count++;
@@ -13,6 +23,8 @@ public class PromedioPares {
             i++;
         }
 
-        return (evens_sum/evens_count);
+        if (evens_count == 0) return 0.0;
+
+        return (evens_sum/(evens_count*1.0));
     }
 }
